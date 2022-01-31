be86cbce6e755ecbac4f8a39b85511400fe4a9cb
kil-jung-keong <76640821+kil-jung-keong@users.noreply.github.com>
2022-01-31 13:52:13 +0900

    Delete 해시_완주하지 못한 선수.py

--- 해시_완주하지 못한 선수.py
+++
@@ -1,6 +0,0 @@
-from collections import Counter
-participant = ["mislav", "stanko", "mislav", "ana"]
-completion = ["stanko", "ana", "mislav"]
-def solution(participant, completion):
-    return list((Counter(participant) - Counter(completion)).keys())[0]
-print(Counter(participant))
